da9325544b31544f0013e49bbc7d9a3d6c1267ac
outwebfeng <amylindsey958@gmail.com>
2024-11-21 01:36:06 +0800

    vercel部署报错： 9:7  Error: Empty components are self-closing  react/self-closing-comp

--- components/seo/SeoScript.tsx
+++ components/seo/SeoScript.tsx
@@ -1,12 +1,15 @@
 import Script from 'next/script';
-
 import { GOOGLE_TRACKING_ID } from '@/lib/env';
 
 export default function SeoScript() {
   return (
     <>
       <Script strategy='afterInteractive' src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TRACKING_ID}`} />
-      <script defer data-domain='holocure.fun' src='https://www.perpetualcalendar.top/js/script.js'></script>
+      <Script
+        strategy='afterInteractive'
+        src='https://www.perpetualcalendar.top/js/script.js'
+        data-domain='holocure.fun'
+      />
       <Script
         id='gtag-init'
         strategy='afterInteractive'
@@ -16,7 +19,7 @@ export default function SeoScript() {
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', '${GOOGLE_TRACKING_ID}', {
-            page_path: window.location.pathname,
+              page_path: window.location.pathname,
             });
           `,
         }}
